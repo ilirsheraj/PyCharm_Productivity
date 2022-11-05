0115cad6bd0224d48d2e2aa89b52c935f5fe1fb1
Like <sherajilir@gmail.com>
2022-11-05 23:09:45 +0100

    Add files via upload

---
+++ Section_01_Basics/main.py
@@ -0,0 +1,13 @@
+# Let's start the usual way
+print("Hello World")
+
+# Variables types in Python
+data = 21
+pi = 3.14
+email = "willsmith@yahoo.com"
+eat = True
+
+print(data)
+print(pi)
+print(email)
+print(eat)
