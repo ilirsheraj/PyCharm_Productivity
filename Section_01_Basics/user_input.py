43d3c15c1f8559f8066570bb1bc07f6d216b0653
Like <sherajilir@gmail.com>
2022-11-05 23:21:34 +0100

    Add files via upload

---
+++ Section_01_Basics/user_input.py
@@ -0,0 +1,8 @@
+# basketballPlayer = input("Who is your favorite basketball player: ")
+#
+# print("My favorite player is " + basketballPlayer)
+
+# Logical Operations
+yourAge = input("How old are you? ")
+birthYear = 2022 - int(yourAge)
+print("You were born in " +  str(birthYear))
