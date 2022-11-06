46d138894f5076669081082c753085e7754fb690
Like <sherajilir@gmail.com>
2022-11-06 01:06:30 +0100

    Add files via upload

---
+++ Section_01_Basics/logical_operators.py
@@ -0,0 +1,13 @@
+x = 5 > 4
+print(x)
+
+x = 5 != 4
+print(x)
+
+x = 5 == 4
+print(x)
+
+rent = 550
+print(rent > 50 or rent < 100)
+print(rent > 50 and rent < 100)
+print(rent > 50 and rent < 1000 and rent != 400)
