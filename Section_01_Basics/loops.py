33f190ce64895a8e3bee071b11db3650a5391a36
Like <sherajilir@gmail.com>
2022-11-06 01:44:29 +0100

    Add files via upload

---
+++ Section_01_Basics/loops.py
@@ -0,0 +1,18 @@
+# While loop
+i = 0
+while i < 5:
+	print(i)
+	print("i" * i)
+	i += 1
+print("=" * 10)
+
+# for loop#
+temperature = [67, 30, 70, 43, 91]
+for item in temperature:
+	print(item)
+print("=" * 10)
+
+# numbers = range(10)
+numbers = range(10, 100, 20)
+for number in numbers:
+	print(number)
